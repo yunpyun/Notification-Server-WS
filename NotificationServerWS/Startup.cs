ea73e36dad72ff074a7becd381cf4e81fcc9764e
yunpyun <kp.flighty@yandex.ru>
2022-12-05 10:48:42 +0300

    Правки логирования
    Убраны лишние параметры для логирования

--- NotificationServerWS/Startup.cs
+++ NotificationServerWS/Startup.cs
@@ -60,7 +60,7 @@ namespace NotificationServerWS
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup> logger)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             _logger.LogInformation("Configure called");
 
